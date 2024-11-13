fdaec93e080b597b49985ca010fa9bf230200daa
conor-b777 <coblount4954@student.pps.net>
2024-11-13 13:39:30 -0800

    working on making the thingy put the cursor in the center (you know what i mean)

--- cursorLook.py
+++ cursorLook.py
@@ -3,11 +3,23 @@ import pyautogui
 import numpy
 
 cursorpos = pyautogui.position()
+halfx = cursorpos.x%2
+halfy = cursorpos.y%2
+
+nposx = cursorpos.x - halfx
+nposy = cursorpos.y - halfy
+
+print(f"{cursorpos}")
 
 while True == True:
-    im = numpy.array(pyautogui.screenshot(region=((cursorpos) + (300, 300))))
+    im = numpy.array(pyautogui.screenshot(region=((nposx, nposy) + (300, 300))))
 
     cursorpos = pyautogui.position()
+    halfx = cursorpos.x%2
+    halfy = cursorpos.y%2
+
+    nposx = cursorpos.x - halfx
+    nposy = cursorpos.y - halfy
 
     cv2.imshow("cursor", im)
 
