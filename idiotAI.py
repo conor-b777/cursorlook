75f80b2c36829b4558b6e031c154d9a3be38a845
conor-b777 <coblount4954@student.pps.net>
2024-11-21 13:07:24 -0800

    not ai cuz w3 is down or whatever

---
+++ idiotAI.py
@@ -0,0 +1,10 @@
+import numpy
+import matplotlib.pyplot
+
+x = numpy.random.uniform(0.0, 5.0, 1000)
+matplotlib.pyplot.hist(x, 100)
+while True == True:
+    matplotlib.pyplot.show()
+    
+    if True == False:
+        break
