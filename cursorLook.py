43a7f96874319a536a4082ab6b97b7d85174740e
conor-b777 <coblount4954@student.pps.net>
2024-12-06 11:53:16 -0800

    made the code easier to modify

--- cursorLook.py
+++ cursorLook.py
@@ -2,17 +2,19 @@ import cv2
 import pyautogui
 import numpy
 
+wwidth = 450
+wheight = 450
 cursorpos = pyautogui.position()
 
 nposx = cursorpos.x - 225
 nposy = cursorpos.y - 225
 
 while True == True:
-    im = numpy.array(pyautogui.screenshot(region=((nposx, nposy) + (450, 450))))
+    im = numpy.array(pyautogui.screenshot(region=((nposx, nposy) + (wwidth, wheight))))
 
     cursorpos = pyautogui.position()
-    nposx = cursorpos.x - 225
-    nposy = cursorpos.y - 225
+    nposx = cursorpos.x - wwidth/2
+    nposy = cursorpos.y - wheight/2
 
     cv2.imshow("Press Q to kill window", im)
 
