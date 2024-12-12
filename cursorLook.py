7275e6e1b845ad0b0b6f328ead1a2d5ab203df44
conor-b777 <coblount4954@student.pps.net>
2024-12-12 13:47:32 -0800

    thank you mr. moye

--- cursorLook.py
+++ cursorLook.py
@@ -11,12 +11,14 @@ nposy = cursorpos.y - wheight/2
 
 while True:
     im = numpy.array(pyautogui.screenshot(region=((nposx, nposy) + (wwidth, wheight))))
+    colorImg = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 
     cursorpos = pyautogui.position()
     nposx = cursorpos.x - wwidth/2
     nposy = cursorpos.y - wheight/2
 
-    cv2.imshow("Press Q to kill window", im)
+    if not nposx < 5 or not nposy < 5:
+        cv2.imshow("Press Q to kill window", colorImg)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
